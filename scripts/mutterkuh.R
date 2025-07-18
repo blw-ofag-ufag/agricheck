d3a2ef09c7cb7ee679955aac6f7ddfccbc735aa9
Damian-Oswald <damian.oswald@protonmail.com>
2025-07-18 15:07:32 +0200

    Integrate mutterkuh data

---
+++ scripts/mutterkuh.R
@@ -0,0 +1,43 @@
+library(rdfhelper)
+
+# read data
+data <- readxl::read_excel("data/mutterkuh.xlsx")
+
+# define URI prefixes
+base   <- "https://agriculture.ld.admin.ch/inspection/"
+rdfs   <- "http://www.w3.org/2000/01/rdf-schema#"
+schema <- "http://schema.org/"
+
+# create file
+sink("rdf/mutterkuh.ttl")
+
+# loop over each row in excel table
+for (i in 1:nrow(data))
+{
+  # define a URI based on hashed entry
+  subject <- rdfhelper::uri(toupper(rlang::hash(data[i,2])), base)
+
+  # define class and parent
+  rdfhelper::triple(subject, "a", rdfhelper::uri("InspectionPoint", base))
+  rdfhelper::triple(
+    subject = subject,
+    predicate = rdfhelper::uri("isPartOf", prefix = schema),
+    object = rdfhelper::uri("84EA1092A8794BB0AFD2B7A486619C7G", prefix = base)
+    )
+
+  # give all the labels and comments
+  for (property in c("label", "comment"))
+  {
+    for (lang in c("de", "fr", "it"))
+    {
+      x <- as.character(data[i, paste(property, lang, sep = "_")])
+      rdfhelper::triple(
+        subject = subject,
+        predicate = rdfhelper::uri(property, prefix = rdfs),
+        object = rdfhelper::langstring(x, lang = lang)
+        )
+    }
+  }
+}
+
+sink()
