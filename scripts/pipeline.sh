eef5a661483fb57fd07b997a36f04d64426708a6
Damian-Oswald <damian.oswald@protonmail.com>
2025-09-02 12:45:21 +0200

    Integrate SwissGAP data (Closes #41)
    - Write own ETL pipeline to convert SwissGAP data to RDF
    - Special treatment: All caps titles are converted to lowercase with
      capitalized start.

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,9 +1,9 @@
 # Run R preprocessing steps
-for r in acontrol bioinspecta mutterkuh qm; do
+for r in acontrol bioinspecta mutterkuh qm swissgap; do
   Rscript "scripts/${r}.R"
 done
 
 # Validate and reason over the RDF
 python3 scripts/validate-syntax.py
-python3 scripts/reason.py rdf/{ontology,bioinspecta,mapping,acontrol,mutterkuh,qm}.ttl
+python3 scripts/reason.py rdf/{ontology,bioinspecta,mapping,acontrol,mutterkuh,qm,swissgap}.ttl
 python3 scripts/validate-shape.py
