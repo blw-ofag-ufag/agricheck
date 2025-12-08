608268b0ade77b3e2e53d283f48e376603f5703d
Damian-Oswald <damian.oswald@protonmail.com>
2025-12-08 19:08:02 +0100

    Implement SwissGAP requested changes

--- scripts/swissgap.R
+++ scripts/swissgap.R
@@ -10,55 +10,45 @@ library(stringr)
 # define RDF prefixes, bases etc.
 base <- "https://agriculture.ld.admin.ch/inspection/"
 
-# Helper function to get slug of ID
-parse_identifier <- function(x)
-{
-  x <- strsplit(x, "\\.") |> unlist() |> as.integer()
-  list(
-    x     = x,
-    slug  = tail(x, 1),
-    level = 3-sum(x==0)
-  )
-}
+# Save SwissGAP IRI
+swissgap <- rdfhelper::uri("B3A2CF324826FC66839483546480AE12", prefix = base)
+
 
 # READ AND PRE-PROCESS DATA FRAME
 # ===============================
 
 # Read Excel file from SwissGAP
-data <- readxl::read_excel("data/swissgap.xlsx", range = readxl::cell_cols("F:I"), skip = 0)
+data <- readxl::read_excel("data/swissgap.xlsx", range = readxl::cell_cols("C:F"), skip = 0)
 
 # Re-assign column names
-colnames(data) <- c("id", "de", "fr", "it")
+colnames(data) <- c("level", "de", "fr", "it")
+
+# Assign an ID based on the Excel order
+data$id <- 1:nrow(data)
+
+# convert hierarchy to int
+data$level <- as.integer(data$level)
 
 # Create an ID for each row in the data set
 data$URI <- NA
-for (i in 1:nrow(data))
-{
-  data[i,"URI"] <- rdfhelper::uri(toupper(rlang::hash(paste0("SwissGAP_",data[i,"id"]))), base)
-}
-
-# Assign a code that allows quick search of a parent
-data$code <- NA
-for (i in 1:nrow(data))
-{
-  x <- parse_identifier(as.character(data[i,"id"]))
-  data[i,"code"] <- paste(x$x[1:x$level], collapse = ".")
+for (i in 1:nrow(data)) {
+  data[i,"URI"] <- rdfhelper::uri(toupper(rlang::hash(paste0("SwissGAP_", data[i,"id"]))), base)
 }
 
 # Determine the parent of each point
 data$parent <- NA
-for (i in 1:nrow(data)) {
+for(i in seq_len(nrow(data))) {
 
-  # Use reg-ex to define parent code
-  # (Each code is showing the hierarchy; e.g. object with code `1.2.3` has parent with code `1.2`)
-  parent_code <- sub("\\.[^.]*$", "", data[i,"code"])
-
-  # Search for the parents URI and insert it
-  data[i,"parent"] <- as.character(subset(data, code==parent_code, select = URI))
+  if (data$level[i]==1) {
+    data$parent[i] <- swissgap
+    one <- data$URI[i]
+  } else if(data$level[i]==2) {
+    data$parent[i] <- one
+    two <- data$URI[i]
+  } else if(data$level[i]==3) {
+    data$parent[i] <- two
+  }
 }
-
-# Save SwissGAP IRI
-swissgap <- rdfhelper::uri("B3A2CF324826FC66839483546480AE12", prefix = base)
 
 # open new RDF file
 sink("rdf/swissgap.ttl")
@@ -67,18 +57,16 @@ sink("rdf/swissgap.ttl")
 for (i in 1:nrow(data)) {
 
   # save current URI
-  uri <- as.character(data[i,"URI"])
+  uri <- as.character(data[i, "URI"])
 
-  # if the identifier slug is 0, it's a collection of inspection points
-  level <- parse_identifier(as.character(data[i,"id"]))$level
+  level <- data$level[i]
 
   # determine class of the current row
   class <- ifelse(level==3, uri("InspectionPoint", base), uri("Collection", "http://purl.org/dc/terms/"))
 
   # class statement
   rdfhelper::triple(uri, "a", class)
-  for (lang in c("de", "fr", "it"))
-  {
+  for (lang in c("de", "fr", "it")) {
     x <- as.character(data[i,lang])
     if (x == stringr::str_to_upper(x))
     {
@@ -92,17 +80,10 @@ for (i in 1:nrow(data)) {
   rdfhelper::triple(uri, uri("http://schema.org/identifier"), literal(as.character(data[i,"id"])))
 
   # parent statement
-  if (level == 1)
-  {
-    rdfhelper::triple(uri, uri("http://schema.org/isPartOf"), swissgap)
-  }
-  else if (level == 2)
-  {
-    rdfhelper::triple(uri, uri("http://schema.org/isPartOf"), as.character(data[i,"parent"]))
-  }
-  else if (level ==3)
-  {
+  if (level == 3) {
     rdfhelper::triple(uri, uri("belongsToGroup", base), as.character(data[i,"parent"]))
+  } else {
+    rdfhelper::triple(uri, uri("http://schema.org/isPartOf"), as.character(data[i,"parent"]))
   }
 }
 
